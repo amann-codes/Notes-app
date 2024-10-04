af56c2d68b2dd6750a42ce16cde060ec8561cf39
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-10-04 23:12:26 +0530

    adding pages, components and navigation

---
+++ src/components/GroupsSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function GroupsSection() {
+  return (
+    <div className='max-w-[500px] bg-white'>
+        <div className='flex flex-col justify-start'>
+            <p>Pocket Notes</p>
+            <div></div>
+        </div>
+    </div>
+  )
+}
